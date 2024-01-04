7c6bf53a8e16945c2ff99b269d71edb7bf8888d2
balikeshwar17 <balikeshwart@gmail.com>
2024-01-05 04:39:39 +0530

    backend modified for signup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 const express=require('express');
 const app=express();
 const PORT = 5000;
+
 app.get('/',(req,res)=>{
     res.send("hello");
 })
+
+app.get('/signup',(req,res)=>{
+    res.send("do signup");
+})
+
 
 
 app.listen(PORT,(req,res)=>{
